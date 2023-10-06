7a8202f44235f6dbd301e692b309524e84e550ff
GMarsura <137362859+GMarsura@users.noreply.github.com>
2023-10-05 21:42:29 -0300

    Update pesoIdeal.java

--- tpa/while-for/pesoIdeal.java
+++ tpa/while-for/pesoIdeal.java
@@ -17,23 +17,23 @@ public static String perguntar() {
 	boolean sexoF;
 
 
-	System.out.println("Qual é seu sexo?");
+	System.out.println("Qual Ã© seu sexo?");
 	sexo = in.next();
 	sexoF = sexo.equalsIgnoreCase("feminino") || sexo.equalsIgnoreCase("femia") || sexo.equalsIgnoreCase("feminina") || sexo.equalsIgnoreCase("mulher") || sexo.equalsIgnoreCase("manina") || sexo.equalsIgnoreCase("f"); 
 
 	if(sexoF) {
-		System.out.println("Qual é a sua altura?");
+		System.out.println("Qual Ã© a sua altura?");
 		altura = in.nextDouble();
 		pesoIdeal = 52 + (0.67 *(altura - 152.4));
 	}else {
-		System.out.println("Qual é a sua altura?");
+		System.out.println("Qual Ã© a sua altura?");
 		altura = in.nextDouble();
 		pesoIdeal = 52 + (0.75 *(altura - 152.4));
 	}
-	System.out.println("O peso em que vc deveria estar de acordo com a sua altura é +/- "+pesoIdeal);
+	System.out.println("O peso em que vc deveria estar de acordo com a sua altura Ã© +/- "+pesoIdeal);
 
 	System.out.println("quer refazer o progama ?");
 	resposta = in.next();
-	return "sim";
+	return resposta;
 }
 }
