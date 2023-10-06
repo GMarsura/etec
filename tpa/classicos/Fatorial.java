55c57cdfc7ff66adbf900a305560f9ada4afd13a
GMarsura <137362859+GMarsura@users.noreply.github.com>
2023-10-05 23:01:30 -0300

    Update Fatorial.java

--- tpa/classicos/Fatorial.java
+++ tpa/classicos/Fatorial.java
@@ -1,5 +1,26 @@
 package classicos;
-
+import java.util.*;
 public class Fatorial {
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
 
+    System.out.print("Digite um n�mero");
+    int numero = in.nextInt();
+
+    long fatorial = 1;
+    int i = 1;
+
+    while (i <= numero) {
+        fatorial *= i;
+        i++;
+    }
+
+    System.out.println("O fatorial de " + numero + " �: " + fatorial);
+
+
+ 
+
+ 
+
+}
 }
