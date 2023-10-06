41106d05810c4df8f182330b5e7b5a78d71fb459
GMarsura <137362859+GMarsura@users.noreply.github.com>
2023-10-05 23:02:42 -0300

    Update AnosAltura.java

--- tpa/classicos/AnosAltura.java
+++ tpa/classicos/AnosAltura.java
@@ -1,5 +1,39 @@
 package classicos;
-
+import java.util.*;
 public class AnosAltura {
+public static void main(String[] args) {
 
+	while(perguntar().equalsIgnoreCase("sim")) {
+
+	}
+
+	System.out.println("perfeitamente");
+
+}
+public static String perguntar() {
+	Scanner in = new Scanner(System.in);
+	String sexo, resposta;
+	double pesoIdeal, altura;
+	boolean sexoF;
+
+
+	System.out.println("Qual � seu sexo?");
+	sexo = in.next();
+	sexoF = sexo.equalsIgnoreCase("feminino") || sexo.equalsIgnoreCase("femia") || sexo.equalsIgnoreCase("feminina") || sexo.equalsIgnoreCase("mulher") || sexo.equalsIgnoreCase("manina") || sexo.equalsIgnoreCase("f"); 
+
+	if(sexoF) {
+		System.out.println("Qual � a sua altura?");
+		altura = in.nextDouble();
+		pesoIdeal = 52 + (67 *(altura - 152.4));
+	}else {
+		System.out.println("Qual � a sua altura?");
+		altura = in.nextDouble();
+		pesoIdeal = 52 + (75 *(altura - 152.4));
+	}
+	System.out.println("O peso em que vc deveria estar de acordo com a sua altura � +/- "+pesoIdeal);
+
+	System.out.println("quer refazer o progama ?");
+	resposta = in.next();
+	return resposta;
+}
 }
