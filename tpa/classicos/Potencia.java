3fa66a6792a82a4c995c5fa7edbe51ef577a0070
GMarsura <137362859+GMarsura@users.noreply.github.com>
2023-10-05 23:03:07 -0300

    Update Potencia.java

--- tpa/classicos/Potencia.java
+++ tpa/classicos/Potencia.java
@@ -1,5 +1,25 @@
 package classicos;
-
+import java.util.*;
 public class Potencia {
+public static void main(String[] args) {
+    int i = 0,base, exp ;
+    Scanner in = new Scanner(System.in);
 
+    System.out.println("digite o valor da base");
+    base = in.nextInt();
+    System.out.println("digite o valor do Expoente");
+    exp = in.nextInt();
+
+    int resultado = 1;
+
+     while(i <= exp) {
+         resultado *= base;
+         i +=1;
+         if(i == exp) {
+             System.out.println(resultado);
+         }
+     }
+
+
+}
 }
